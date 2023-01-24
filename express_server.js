4b22ca20b13661b83cc27712e5a53d4b2a9e7607
Amaal Ali <amaalalikhadija@gmail.com>
2023-01-23 20:05:38 -0500

    finallized some changes to the website

--- express_server.js
+++ express_server.js
@@ -9,9 +9,13 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 app.post("/urls", (req, res) => {
+  const key = generateRandomString();
+  const longURL = req.body.longURL
+  urlDatabase[key] = longURL
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${key}`); // Respond with 'Ok' (we will replace this)
 });
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -28,9 +32,14 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index.ejs", templateVars);
 });
+// route is /:id is parameters in its path 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+app.get("/u/:id", (req, res) => {
+ const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 
@@ -40,6 +49,5 @@ app.listen(PORT, () => {
 
 function generateRandomString() {
   const result = Math.random().toString(36).substring(2,8);
-  console.log(result);
+  return result;
 }
-generateRandomString();
